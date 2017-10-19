3991d631b6e2be764c3d07807986a23f217df3de
MumtazAkhtar <mumtazmohammed08@gmail.com>
2017-10-19 21:25:39 +0200

    added node components to website

--- README.md
+++
@@ -1,2 +0,0 @@
-# BusinessWebsite
-m&amp;m restaurant website
---
+++ index.js
@@ -0,0 +1,21 @@
+var express = require("express");
+var app = express();
+app.use(express.static('public'));
+
+app.get('/', function(req,res){
+	res.sendFile('index.html')
+})
+app.get('/aboutus', function(req,res){
+	res.sendFile('about-us-page.html')
+})
+app.get('/menu', function(req,res){
+	res.sendFile('Imenu.html')
+})
+app.get('/ordernow',function(req,res){
+	res.sendFile('order-now.html')
+})
+
+app.listen(3000, function(){
+	console.log('running at port 3000');
+
+})
